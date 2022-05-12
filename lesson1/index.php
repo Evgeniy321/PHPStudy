b37185a67040a557ad424eff1343d7471c5380f7
Evgeniy321 <81414292+Evgeniy321@users.noreply.github.com>
2022-05-12 16:53:19 +0500

    added new file second

--- lesson1/index.php
+++ lesson1/index.php
@@ -4,11 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>first site</title>
+    <link rel="stylesheet" href="styles/main.css">
 </head>
 <body>
+    
+    <!-- 
+        коментарий на html
+    -->
     <?php
-    echo "Hello, world!";
+    if(mt_rand(0,1)){
+        echo "<div class=\"blue block\">Blue text</div>";//первый коментарий на php
+    }else{
+        echo "<div class=\"red block\">Red text</div>";# второй коментарий на php
+    }
+    require 'second.php'
+    // выдает ошибку, если такого файла не обнаружится 
+    // быстрые клавиши для добавления коментария на VSCode Ctrl + /
+    /*
+    многострочный коментарий на php
+    */
     ?>
 </body>
 </html>
